a210881c5229d0ae34cf550a6018c959fca66241
RBeltran12358 <Ricardo.beltran12358@gmail.com>
2022-06-01 13:25:29 -0700

    Changes

--- InnReservations.java
+++ InnReservations.java
@@ -1,5 +1,5 @@
-import java.util.*;
-
-public static void main(String[] args) {
-    System.out.println("Hello There");
+public class InnReservations {
+    public static void main(String[] args) {
+        System.out.println("Hello There");
+    }
 }
