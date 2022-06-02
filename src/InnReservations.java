a23edff5decd6ba0365c5a0e9b1127180b2d0889
RBeltran12358 <Ricardo.beltran12358@gmail.com>
2022-06-01 17:06:38 -0700

    Able to print out contents from a column in our remote RDBMS.

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -1,10 +1,51 @@
+import java.sql.*;
 import java.util.Scanner;
 
 
 public class InnReservations {
 
-    public static void RoomsAndRates() {
-        System.out.println("1");
+    public static void RoomsAndRates() throws SQLException{
+        System.out.println("Room and Rates");
+
+        // Step 0: Load MySQL JDBC Driver
+        // No longer required as of JDBC 2.0  / Java 6
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("MySQL JDBC Driver loaded");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Unable to load JDBC Driver");
+            System.exit(-1);
+        }
+
+        // Step 1: Establish connection to RDBMS
+        try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
+                System.getenv("HP_JDBC_USER"),
+                System.getenv("HP_JDBC_PW"))) {
+
+            // Step 2: Construct SQL statement
+            String sql = "select RoomName from rbeltr01.lab7_rooms";
+
+            // Step 3: (omitted in this example) Start transaction
+            try (Statement stmt = conn.createStatement()) {
+
+                // Step 4: Send SQL statement to DBMS
+                boolean exRes = stmt.execute(sql);
+                ResultSet res = stmt.executeQuery(sql);
+
+
+                while(res.next()){
+                    System.out.println(res.getString("RoomName"));
+                }
+
+
+
+                // Step 5: Handle results
+                System.out.format("Result from ALTER: %b %n", exRes);
+            }
+
+            // Step 6: (omitted in this example) Commit or rollback transaction
+        }
+        // Step 7: Close connection (handled by try-with-resources syntax)
     }
 
     public static void Reservations() {
@@ -27,10 +68,7 @@ public class InnReservations {
         System.out.println("6");
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int response = 0;
-
+    public static void printIntro() {
         System.out.println("Welcome to our database!");
         System.out.println("To select an option from the list below, type the number to its right and press Enter\n");
         System.out.println("Rooms and Rates: 1");
@@ -40,33 +78,44 @@ public class InnReservations {
         System.out.println("Detailed Reservation Information: 5");
         System.out.println("Revenue: 6");
         System.out.println("Quit Application: 7\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int response = 0;
+        InnReservations ir = new InnReservations();
+        printIntro();
 
         while(true){
             try {
                 response = in.nextInt();
+                if(response == 1){
+                    RoomsAndRates();
+                } else if(response == 2){
+                    Reservations();
+                } else if(response == 3){
+                    ReservationChange();
+                } else if(response == 4){
+                    ReservationCancellation();
+                } else if(response == 5){
+                    DetailedReservationInformation();
+                } else if(response == 6){
+                    Revenue();
+                } else if(response == 7){
+                    break;
+                }else{
+                    System.out.println("Number not in range of options");
+                }
             }
-            catch(Exception e) {
-                System.out.println("Wrong format: Please input a digit from 1-7 inclusive and press Enter");
-                break;
+            catch (SQLException e) {
+                System.err.println("SQLException: " + e.getMessage());
+            }
+            catch (Exception e2) {
+//                System.out.println("Wrong format: Please input a digit from 1-7 inclusive and press Enter");
+                System.err.println("Exception: " + e2.getMessage());
             }
 
-            if(response == 1){
-                RoomsAndRates();
-            } else if(response == 2){
-                Reservations();
-            } else if(response == 3){
-                ReservationChange();
-            } else if(response == 4){
-                ReservationCancellation();
-            } else if(response == 5){
-                DetailedReservationInformation();
-            } else if(response == 6){
-                Revenue();
-            } else if(response == 7){
-                break;
-            }else{
-                System.out.println("Number not in range of options");
-            }
+
         }
         System.out.println("Exiting Application :)");
     }
